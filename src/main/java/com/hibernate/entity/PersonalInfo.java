507d082e31a0b925a12f76f062bfeddc3ed97501
ybainazar <dom8152daleN!>
2022-06-05 20:19:31 +0600

    new

--- src/main/java/com/hibernate/entity/PersonalInfo.java
+++ src/main/java/com/hibernate/entity/PersonalInfo.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
+import javax.validation.constraints.NotNull;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -23,5 +24,6 @@ public class PersonalInfo implements Serializable {
     private String firstname;
     private String lastname;
 //    private Birthday birthDate;
+    @NotNull
     private LocalDate birthDate;
 }
